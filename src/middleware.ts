8072920b4638fec0a64d95755a91247f6264dd87
Dichan <maxpilot@tyakkai.com>
2025-04-20 16:47:57 +0545

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
-import { verify } from "jsonwebtoken";
+import { jwtVerify } from "jose";
 
 const PUBLIC_ROUTES = ["/sign-in", "/sign-up"]; // Add your public routes here
 const JWT_SECRET = process.env.JWT_SECRET!;
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   // Allow public routes
@@ -17,9 +17,9 @@ export function middleware(req: NextRequest) {
   if (!token) {
     return NextResponse.redirect(new URL("/sign-in", req.url));
   }
-  console.log("Token:", token);
+
   try {
-    verify(token, JWT_SECRET);
+    await jwtVerify(token, new TextEncoder().encode(JWT_SECRET));
     // You can optionally pass decoded info via headers or request context
     return NextResponse.next();
   } catch (err) {
@@ -30,10 +30,5 @@ export function middleware(req: NextRequest) {
 
 // Apply middleware to specific routes
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|sign-in|sign-up).*)"],
 };
