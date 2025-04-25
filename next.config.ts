981f1b62a18ca7403545d67f07a5b26373c27202
Dichan <maxpilot@tyakkai.com>
2025-04-25 12:13:52 +0545

    fix: File size increased for next config

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,17 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["localhost", "res.cloudinary.com"],
   },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "5mb",
+  api: {
+    bodyParser: {
+      sizeLimit: "10mb", // adjust higher than your largest base64 payload
     },
   },
+
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://3.26.41.86/:path*", // 👈 AWS API IP
+        destination: "http://54.66.132.165/:path*", // 👈 AWS API IP
       },
     ];
   },
